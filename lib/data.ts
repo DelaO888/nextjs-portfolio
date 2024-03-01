723ce7c6a5adb7a32c2b8e2932ef3f384b82cb85
DelaO888 <ilovehersubmit@gmail.com>
2024-03-01 13:50:47 -0600

    change image filename

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,6 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 import youtube from "@/public/youtube.png"
 import conmutador from "@/public/conmutador.png"
 import calculator from "@/public/calc.png"
